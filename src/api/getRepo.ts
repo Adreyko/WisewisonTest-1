cd072a6586806b3d7fe04e22aff2c33362759d9f
Andrew <103659333+Adreyko@users.noreply.github.com>
2023-02-25 20:30:31 +0200

    env fix

--- src/api/getRepo.ts
+++ src/api/getRepo.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const getRepo = (userName: string) =>
   axios.get(url + userName, {
     headers: {
-      Authorization: process.env.ACCES_TOKEN,
+      Authorization: process.env.REACT_APP_ACCES_TOKEN,
     },
   });
